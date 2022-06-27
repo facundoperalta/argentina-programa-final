c8367c6290b8ee24a632f941af63fdf1f6ca106b
unknown <you@example.com>
2022-06-26 22:08:04 -0300

    header, navbar offcanvas

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarTogglerComponent } from './components/header/navbar-toggler/navbar-toggler.component';
+import { NavbarOffcanvasComponent } from './components/header/navbar-offcanvas/navbar-offcanvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarTogglerComponent,
+    NavbarOffcanvasComponent
   ],
   imports: [
     BrowserModule,
---
+++ portfolio/src/app/components/header/navbar-offcanvas/navbar-offcanvas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-offcanvas',
+  templateUrl: './navbar-offcanvas.component.html',
+  styleUrls: ['./navbar-offcanvas.component.css']
+})
+export class NavbarOffcanvasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
