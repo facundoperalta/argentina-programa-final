39cb18849bee67d217369d9b05276c9e83bfa4f1
unknown <you@example.com>
2022-07-05 21:30:19 -0300

    educación

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavbarOffcanvasComponent } from './components/header/navbar-offcanvas/n
 import { BannerComponent } from './components/header/banner/banner.component';
 import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ExperienceComponent } from './components/experience/experience.componen
     NavbarOffcanvasComponent,
     BannerComponent,
     AboutComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
